bb1b52b35fcc810134f3093ef329c16709a51a0a
D.J Zou <bopy2008@163.com>
2017-08-28 17:01:08 +0800

    init file

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from Tkinter import *
+
+def createUI():
+    """docstring for createUI"""
+    win = Tk()
+    win.title("FFTools")
+    win.geometry("400x200")
+    
+
+def main():
+    """docstring for main"""
+    createUI()
+
+    mainloop()
+    print("main done")
+
+    
+if __name__ == '__main__':
+    main()
