f7f384cef9bcc9fb67bbcc89ef31d8427bbf6941
D.J Zou <bopy2008@163.com>
2017-08-28 17:58:48 +0800

    start file structure

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from Tkinter import *
+from Tkconstants import *
+from tkFileDialog import *
+
+class ToolApp(object):
+    """docstring for ToolApp"""
+    def __init__(self, arg):
+        super(ToolApp, self).__init__()
+        self.arg = arg
+        frame = Frame(arg)
+        frame.pack()
+        self.button = Button(frame, text="start", command=self.start)
+        self.button.pack(side=LEFT)
+
+        self.selectSourceButton = Button(frame, text="选择源文件", command=self.selectSourceFile)
+        self.selectSourceButton.pack(side=LEFT)
+    def start(self):
+        """docstring for start"""
+        print("begin to start")
+
+    def selectSourceFile(self):
+        """docstring for selectSourceFile"""
+        self.openfilename = askopenfilename()
+        
+        
 
 def createUI():
     """docstring for createUI"""
     win = Tk()
     win.title("FFTools")
     win.geometry("400x200")
+    # openfilename = askopenfilename()
+    app = ToolApp(win)
+    # print(openfilename)
+    win.mainloop()
     
 
 def main():
     """docstring for main"""
     createUI()
-
-    mainloop()
     print("main done")
 
     
